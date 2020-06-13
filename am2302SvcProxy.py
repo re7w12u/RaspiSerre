73e7b7ceb990a1dee28403db669c0114752eee85
re7w12u <bessiere@gmail.com>
2020-06-13 18:46:59 +0200

    add temperature sensor

---
+++ am2302SvcProxy.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# trigger last measure pick up upon am2302.service request
+
+
+import AM2302 as sensor
+sensor.CheckSensor()
+
+
+
--- server.js
+++ server.js
@@ -2,23 +2,48 @@
 
 var express = require("express");
 var app = express();
-
+var Gpio = require("onoff").Gpio;
+var deferred =  require('deferred');
 var fs = require('fs');
-var currentPosition = "";
-fs.readFile(__dirname + "/position.txt", 'utf8', (err, data) => {
-    currentPosition = data;
-});
+
 
 var log = [];
-fs.readFile(__dirname + "/log.txt", 'utf8', (err, data) => {
-    log = data.split('\n');
-});
-
-var Gpio = require("onoff").Gpio;
+var currentPosition = "";
 var extend = new Gpio(10, 'out');
 var retract = new Gpio(25, 'out');
 
 app.use(express.static('public'));
+
+
+fs.readFile(__dirname + "/position.txt", 'utf8', (err, data) => {
+    currentPosition = data;
+});
+
+
+fs.readFile(__dirname + "/log.txt", 'utf8', (err, data) => {
+    log = data.split('\r\n');
+});
+
+function GetAM2302Data(){
+    var am2302 = {
+        'LastMeasureTime':"",
+        'temperature':"",
+        'humidity':""
+    };
+
+    var d = new deferred();
+    fs.readFile(__dirname + "/am2302.csv", 'utf8', (err, data) =>{        
+        lines = data.split('\r\n').filter((e) => e != '');             
+        lastline = lines.slice(-1)[0];        
+        measure = lastline.split(',');        
+        am2302.LastMeasureTime = measure[0] + " - " + measure[1];
+        am2302.temperature = measure[3];
+        am2302.humidity = measure[2];        
+        d.resolve(am2302);
+    });
+
+    return d.promise;
+}
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
@@ -27,8 +52,9 @@ app.get("/", (req, res) => {
 app.get("/log/:lines", (req, res) => {
     var lines = req.params.lines;
     var result = [];
+    
     if (lines != undefined) {
-        result = log.slice(log.length - lines);
+        result = log.slice(log.length - lines).reverse();
     } else {
         result = log;
     }
@@ -37,8 +63,15 @@ app.get("/log/:lines", (req, res) => {
     res.send(JSON.stringify({ log: result }));
 });
 
-app.get("/position", (req, res) => {
-    console.log("current position = " + currentPosition);
+app.get("/am2302", (req, res) => {
+    GetAM2302Data().then((data) =>{
+        res.setHeader('Content-Type', 'application/json');        
+        res.send(JSON.stringify({ temperature: data.temperature, humidity : data.humidity, time : data.LastMeasureTime }));
+    });
+    
+});
+
+app.get("/position", (req, res) => {    
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ currentPosition: currentPosition }));
 });
