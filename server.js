603cb8555fa412dfe7b0f3813289c13518c0745b
re7w12u <bessiere.julien@gmail.com>
2020-08-30 18:28:38 +0200

    add auth

--- server.js
+++ server.js
@@ -11,12 +11,14 @@ import favicon from 'serve-favicon';
 import path from 'path';
 import { spawn } from 'child_process';
 
+
+const adminKeys = { login: "julien", password: "julien" };
 var app = express();
 app.use(express.json()); // use to parse post json requests
 app.use('/api', routers);
 
 //const url = require('url');
-const host = new URL('https://greenhouse.syntax-error.fr/');
+const host = new URL('https://***************.fr/');
 const { Gpio } = pkg;
 //const { deferred } = dPkg;
 const __dirname = path.resolve();
@@ -25,8 +27,8 @@ var log = [];
 var currentPosition = "";
 var extend = new Gpio(10, 'out');
 var retract = new Gpio(25, 'out');
-const parameterFilePath = path.join(__dirname ,'parameters','param.json');
-var params = JSON.parse(fs.readFileSync(parameterFilePath, {encoding : 'utf8'}));
+const parameterFilePath = path.join(__dirname, 'parameters', 'param.json');
+var params = JSON.parse(fs.readFileSync(parameterFilePath, { encoding: 'utf8' }));
 
 app.use(express.static('public'));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -35,18 +37,36 @@ fs.readFile(path.join(__dirname, params.PositionFile), 'utf8', (err, data) => {
     currentPosition = data;
 });
 
+function requireLogin(req, res, next) {    
+    if (Object.keys(req.query).length == 0 && req.query.username == null && req.query.password == null) {        
+        res.redirect("/login");
+    }
+
+    if (req.query.username === adminKeys.login && req.query.password == adminKeys.password) {
+        next();        
+    }
+    else {
+        res.status(401).send();
+    }
+}
+
+
 /* ------------ PAGES -----------------*/
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/pages/index.html");
 });
 
-app.get("/param", (req, res) => {
+app.get("/param", requireLogin, (req, res) => {
     res.sendFile(__dirname + "/pages/admin/param.html");
 });
 
 app.get("/menu", (req, res) => {
     res.sendFile(__dirname + "/pages/menu.html");
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(__dirname + "/pages/login.html");
 });
 
 var server = app.listen(8081, () => {
